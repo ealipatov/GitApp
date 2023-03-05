940650bd25b5f0aee51d8a968e027f7c3a245ed7
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-03-05 13:37:50 +0300

    Lesson01. Создали шаблон для списка пользователей и кнопку для обновления списка.

--- app/src/main/java/by/ealipatov/gitapp/MainActivity.kt
+++ app/src/main/java/by/ealipatov/gitapp/MainActivity.kt
@@ -2,10 +2,18 @@ package by.ealipatov.gitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.ealipatov.gitapp.databinding.ActivityMainBinding
+import by.ealipatov.gitapp.utils.toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.refreshButton.setOnClickListener{
+            toast("Button pressed")
+        }
     }
 }
